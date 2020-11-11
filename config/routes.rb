5429275b5e473d41c6b6095ea31c1ed160abedbf
Oscar Dealtry <odealtry@gmail.com>
2020-11-11 15:17:37 +0000

    restaurants actions done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'restaurants', to: 'restaurants#index'
+  post 'restaurants', to: 'restaurants#create'
+  get 'restaurants/new', to: 'restaurants#new'
+  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
 end
